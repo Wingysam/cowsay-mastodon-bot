29cdcb681189c0765039e5af15ed28ee4930844c
Tobias <tobigames200@gmail.com>
2018-10-14 09:55:57 +0200

    Black font, white background

--- cmd2png.sh
+++ cmd2png.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-$1 | convert -background black -fill white \
+$1 | convert -background white -fill black \
 -font "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf" -pointsize 32 \
 -border 10 -bordercolor black \
 label:@- out.png
