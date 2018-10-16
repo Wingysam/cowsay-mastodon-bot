3c1cda1bd9d59d135d3f569e5e8e7886dd7f38dc
tobias <TheDevMinerTV@gmail.com>
2018-10-16 13:47:42 +0200

    Added a fixed width

--- cmd2png.sh
+++ cmd2png.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-$1 | convert -background white -fill black \
+$1 -w 128 | convert -background white -fill black \
 -font "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf" -pointsize 32 \
 -border 10 -bordercolor black \
 label:@- out.png
